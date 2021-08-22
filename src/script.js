6f17c7fdc93a20abea1f27d16105e90c5d8a5a23
purikura09 <purikura09@gmail.com>
2021-08-22 14:48:33 +0200

    update API

--- src/script.js
+++ src/script.js
@@ -6,8 +6,12 @@ function displayTemperature(response) {
   let mainTemp = document.querySelector("h2");
   let cityElement = document.querySelector("#city");
   let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   mainTemp.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
 }
 axios.get(apiUrl).then(displayTemperature);
